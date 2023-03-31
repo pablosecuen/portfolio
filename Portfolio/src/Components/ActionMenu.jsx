1bc35f33efe6c232d1e87194b24a80297c75e8da
pablosecuen@gmail.com <pablosecuen@gmail.com>
2023-03-31 17:59:26 -0300

    asd

--- Portfolio/src/Components/ActionMenu.jsx
+++ Portfolio/src/Components/ActionMenu.jsx
@@ -7,12 +7,24 @@ function ActionMenu() {
   return (
     <div className="md:absolute md:top-96 md:flex md:h-1/2 md:w-full md:justify-end md:pr-4">
       <div className="flex flex-col">
-        <a href="https://www.linkedin.com/in/pablo-j-amico/"></a>
-        <img src={linkedin} className="h-0 w-0 md:my-4 md:h-12 md:w-12"></img>
-        <a href="https://github.com/pablosecuen"></a>
-        <img src={github} className="h-0 w-0 md:my-4 md:h-12 md:w-12"></img>
-        <a href="https://wa.me/+5493417206026"></a>
-        <img src={whatsapp} className="h-0 w-0 md:my-4 md:h-12 md:w-12"></img>
+        <a href="https://www.linkedin.com/in/pablo-j-amico/">
+          <img
+            src={linkedin}
+            className="h-0 w-0 cursor-pointer md:my-4 md:h-12 md:w-12"
+          ></img>
+        </a>
+        <a href="https://github.com/pablosecuen">
+          <img
+            src={github}
+            className="h-0 w-0 cursor-pointer md:my-4 md:h-12 md:w-12"
+          ></img>
+        </a>
+        <a href="https://wa.me/+5493417206026">
+          <img
+            src={whatsapp}
+            className="h-0 w-0 cursor-pointer md:my-4 md:h-12 md:w-12"
+          ></img>
+        </a>
       </div>
     </div>
   );

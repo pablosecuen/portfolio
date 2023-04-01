466ba0027647226b18cc3b5cb9d772e10435ee93
pablosecuen@gmail.com <pablosecuen@gmail.com>
2023-04-01 13:54:26 -0300

    testing perfomance

--- Portfolio/src/main.jsx
+++ Portfolio/src/main.jsx
@@ -4,6 +4,17 @@ import App from "./App";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import { ParallaxProvider } from "react-scroll-parallax";
+import { getCLS, getFID, getLCP } from "web-vitals";
+
+function sendToAnalytics(metric) {
+  // Send the metric data to your analytics service
+  console.log(metric);
+}
+
+// Measure Core Web Vitals metrics
+getCLS(sendToAnalytics);
+getFID(sendToAnalytics);
+getLCP(sendToAnalytics);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>

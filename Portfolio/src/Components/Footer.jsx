ab0732e2bcfecc6c6c620ae9d6e5920eae5dd449
pablosecuen@gmail.com <pablosecuen@gmail.com>
2023-04-04 20:34:26 -0300

    accessibility fix 3

--- Portfolio/src/Components/Footer.jsx
+++ Portfolio/src/Components/Footer.jsx
@@ -24,7 +24,7 @@ function Footer(props) {
     <div className="flex-col border-2 border-white">
       <div className="flex h-full w-full flex-col items-center align-middle sm:h-48 sm:flex-row">
         <div className="flex w-1/3 flex-col items-center justify-center align-middle">
-          <img src={logo} className="h-24 pt-4 sm:pt-0 md:h-32" />
+          <img src={logo} className="h-24 pt-4 sm:pt-0 md:h-32" alt="logo" />
         </div>
         <div className="flex flex-row items-center gap-4 border-white sm:w-1/3 sm:flex-col sm:gap-0 sm:border-l-2  ">
           {language === "en" ? (

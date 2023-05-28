17195e5a93cc51f8fd09ee54d3edb12f37b77c60
pablosecuen@gmail.com <pablosecuen@gmail.com>
2023-05-28 16:25:56 -0300

    agregue vercel analitics

--- Portfolio/src/App.jsx
+++ Portfolio/src/App.jsx
@@ -2,7 +2,7 @@ import { lazy, Suspense } from "react";
 import { Routes, Route, useLocation } from "react-router-dom";
 
 import Loader from "./Components/Loader";
-
+import { Analytics } from "@vercel/analytics/react";
 import "./App.css"; // import your custom CSS styles
 import NavBar from "./Components/NavBar";
 
@@ -15,6 +15,7 @@ function App() {
       <Suspense fallback={<Loader />}>
         <Routes>
           <Route path="/" element={<Landing />} />
+          <Analytics />
           {/* <Route path="*" element={<NotFound />} /> */}
         </Routes>
       </Suspense>

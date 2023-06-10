c4fa47e70530e676b43466409ad22481824d6cdb
pablosecuen@gmail.com <pablosecuen@gmail.com>
2023-06-10 13:53:37 -0300

    asd

--- Portfolio/src/main.jsx
+++ Portfolio/src/main.jsx
@@ -3,12 +3,12 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
-import { ParallaxProvider } from "react-scroll-parallax";
+
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
-    <ParallaxProvider>
+
       <App />
-    </ParallaxProvider>
+
   </BrowserRouter>
 );
